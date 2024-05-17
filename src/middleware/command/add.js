e96233304e4079765b0d52af870e20646c4afba7
Ivan Shalaginov <shalaginov.dev@gmail.com>
2024-05-18 00:12:44 +0300

    git ignore

--- src/middleware/command/add.js
+++ src/middleware/command/add.js
@@ -4,7 +4,7 @@ const db = require('../../connection/db.connection')
 const Phone = require('../../model/phone')
 
 module.exports = bot.command('add', async ctx => {
-	// db.authenticate().catch(error => console.error(error))
+	db.authenticate().catch(error => console.error(error))
 	console.log('phone')
 	await ctx.reply('Phone added')
 })
